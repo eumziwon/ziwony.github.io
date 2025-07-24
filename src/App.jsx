a8479f68ae3cb7c955fbbcb6fe12fadc1f1c89e4
beatziwon <ziwon1986@gmail.com>
2025-07-24 09:43:50 +0900

    feat: add sortType context and apply to Home and DiaryList

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,21 @@ import Header from "./components/Header";
 const mockData = [
   {
     id: 1,
-    createDate: new Date().getTime(),
+    createdDate: new Date("2025-07-24").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createDate: new Date().getTime(),
+    createdDate: new Date("2025-07-23").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2025-06-09").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -39,20 +45,20 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   // 새로운 일기 추가
-  const onCreate = (createDate, emotionId, content) => {
+  const onCreate = (createdDate, emotionId, content) => {
     dispatch({
       type: "CREATE",
       data: {
         id: idRef.current++,
-        createDate,
+        createdDate,
         emotionId,
         content,
       },
@@ -60,12 +66,12 @@ function App() {
   };
 
   // 기존 일기 수정
-  const onUpdate = (id, createDate, emotionId, content) => {
+  const onUpdate = (id, createdDate, emotionId, content) => {
     dispatch({
       type: "UPDATE",
       data: {
         id,
-        createDate,
+        createdDate,
         emotionId,
         content,
       },
@@ -84,11 +90,26 @@ function App() {
       <DiaryStateContext.Provider value={data}>
         <DiaryDispatchContext value={{ onCreate, onUpdate, onDelete }}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new" element={<New />} />
-            <Route path="/diary/:id" element={<Diary />} />
-            <Route path="/edit/:id" element={<Edit />} />
-            <Route path="*" element={<Notfound />} />
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/new"
+              element={<New />}
+            />
+            <Route
+              path="/diary/:id"
+              element={<Diary />}
+            />
+            <Route
+              path="/edit/:id"
+              element={<Edit />}
+            />
+            <Route
+              path="*"
+              element={<Notfound />}
+            />
           </Routes>
         </DiaryDispatchContext>
       </DiaryStateContext.Provider>
--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,22 +1,56 @@
 import "./DiaryList.css";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const DiaryList = () => {
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedDate = () => {
+    return data.toSorted((a, b) => {
+      // toSorted를 사용하는 이유 => sort 매소드는 어떠한 값도 반환하지 않고 원본 배열을 정렬하는 기능이다
+      // toSorted는 원본 배열은 그대로 두고 새로운 배열을 반환한다.
+      // 원본 배열을 수정하게 되면 문제가 생기기 때문에 toSorted를 사용한다.
+
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedDate = getSortedDate();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된 순</option>
         </select>
         <Button
+          onClick={() => {
+            nav("/new");
+          }}
           text={"새 일기 쓰기"}
           type={"POSITIVE"}
         />
       </div>
       <div className="list_wrapper">
-        <DiaryItem />
+        {sortedDate.map((item) => (
+          <DiaryItem
+            key={item.id}
+            {...item}
+          />
+        ))}
       </div>
     </div>
   );
