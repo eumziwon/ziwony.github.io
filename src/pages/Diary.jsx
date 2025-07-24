a1f07408950ab35da0151d7e1987fc54b733c556
beatziwon <ziwon1986@gmail.com>
2025-07-24 18:01:19 +0900

    wip: create initial viewer component

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,10 +1,27 @@
 import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+import { useNavigate } from "react-router-dom";
 
 const Diary = () => {
   const params = useParams();
-  console.log(params);
-
-  return <div>{params.id}번 Diary</div>;
+  const nav = useNavigate();
+  return (
+    <div>
+      <Header
+        title={"yyyy-mm-dd 기록"}
+        leftChild={
+          <Button
+            onClick={() => nav(-1)}
+            text={"< 뒤로가기"}
+          />
+        }
+        rightChild={<Button text={"수정하기"} />}
+      />
+      <Viewer />
+    </div>
+  );
 };
 
 export default Diary;
